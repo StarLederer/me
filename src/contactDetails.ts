3a54429623be299bc8f73c71cea315b2f861ef7f
Herman Lederer <germans.lederers@gmail.com>
2023-01-29 18:05:09 +0100

    Improve contact details

---
+++ src/contactDetails.ts
@@ -0,0 +1,33 @@
+export type ContactMethod = "matrix" | "phone" | "email" | "address";
+
+export type ContactDetail = {
+  method: ContactMethod;
+  url?: string;
+  data: string;
+};
+
+export type ContactDetails = ContactDetail[];
+
+export const contactDetails: ContactDetails = [
+  {
+    method: "phone",
+    data: "+31 6 83968615",
+    url: "tel: +31 6 83968615",
+  },
+  {
+    method: "matrix",
+    url: "https://matrix.to/#/@starlederer:matrix.org",
+    data: "@starlederer:matrix.org",
+  },
+  {
+    method: "email",
+    url: "mailto: germans.lederers@gmail.com",
+    data: "germans.lederers@gmail.com",
+  },
+  {
+    method: "address",
+    data: "Groningen, Netherlands",
+  },
+];
+
+export default contactDetails;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import contact from "./contactDetails.ts";
 import experience from "./experience.ts";
 import skills from "./skills.ts";
 import education from "./education.ts";
@@ -6,18 +7,14 @@ export const config = {
   name: "Star Lederer",
   subtitle: "Innovator, open-source enthusiast, UX/DX perfectionist",
   portfolioUrl: "starlederer.github.io",
-  contact: [
-    "+31 6 83968615",
-    "@starlederer:matrix.org",
-    "germans.lederers@gmail.com",
-    "Groningen, Netherlands",
-  ],
+  contact,
   experience,
   skills,
   education,
 };
 
-export * from "./experience.ts"
-export * from "./skills.ts"
-export * from "./experience.ts"
+export * from "./contactDetails.ts";
+export * from "./experience.ts";
+export * from "./skills.ts";
+export * from "./experience.ts";
 export default config;
